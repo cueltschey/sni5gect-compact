0cc46affe24908990242e4ab6a7463f056c076e4
Charles Ueltschey <chaseuelt@gmail.com>
2026-03-12 12:27:05 -0500

    Another update

--- worker_thread.py
+++ worker_thread.py
@@ -7,7 +7,6 @@ class sni5gect(WorkerThread):
         self.cleanup_old_containers()
         self.setup_env()
         self.setup_networks()
-
         self.config.container_volumes[self.config.config_file] = {"bind": "/sni5gect.yaml", "mode": "ro"}
         self.config.container_volumes["/home/charles/collected_iq"] = {"bind": "/iq", "mode": "ro"}
         self.setup_volumes()
