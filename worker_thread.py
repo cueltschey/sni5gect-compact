b23514d09e708f303ad4501b6a61c99cb1ddb8ef
Charles Ueltschey <chaseuelt@gmail.com>
2026-03-12 12:24:32 -0500

    Update worker thread

--- worker_thread.py
+++ worker_thread.py
@@ -1,16 +1,13 @@
-from worker_thread import WorkerThread
-
 class sni5gect(WorkerThread):
     def start(self):
         self.config.image_name = "ghcr.io/cueltschey/sni5gect-compact"
+
         self.cleanup_old_containers()
         self.setup_env()
         self.setup_networks()
+
         self.config.container_volumes[self.config.config_file] = {"bind": "/sni5gect.yaml", "mode": "ro"}
         self.config.container_volumes["/home/charles/collected_iq"] = {"bind": "/iq", "mode": "ro"}
         self.setup_volumes()
 
         self.start_container()
-
-
-
